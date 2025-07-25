5d9e5e5642179dd266bb72e2df972a5f107cef3d
zhangjianshe <zhangjianshe@gmail.com>
2025-07-25 22:21:45 +0800

    opt:fix project  path

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract current version from main.go
-CURRENT_VERSION=$(grep 'var AppVersion =' cis-server/main.go | cut -d'"' -f2)
+CURRENT_VERSION=$(grep 'var AppVersion =' ./cs-server/main.go | cut -d'"' -f2)
 
 # Increment version (assuming semantic versioning)
 IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
@@ -11,10 +11,10 @@ NEW_PATCH=$((PATCH + 1))
 NEW_VERSION="${MAJOR}.${MINOR}.${NEW_PATCH}"
 
 # Update version in main.go
-sed -i "s/var AppVersion = \"${CURRENT_VERSION}\"/var AppVersion = \"${NEW_VERSION}\"/" cis-server/main.go
+sed -i "s/var AppVersion = \"${CURRENT_VERSION}\"/var AppVersion = \"${NEW_VERSION}\"/" cs-server/main.go
 
 # Commit changes
-git add cis-server/main.go
+git add cs-server/main.go
 git commit -m "Bump version to ${NEW_VERSION}"
 
 # Push changes
