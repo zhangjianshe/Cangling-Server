5eb14a28304556ed93aa3c6e6fca1a359618b096
zhangjianshe <zhangjianshe@gmail.com>
2025-07-26 01:04:03 +0800

    opt:fix release action

--- cs-server/main.go
+++ cs-server/main.go
@@ -23,7 +23,7 @@ import (
 //go:embed static/*
 var staticFiles embed.FS
 
-var AppVersion = "0.0.69"             // Current application version
+var AppVersion = "0.0.64"             // Current application version
 var BuildTime = "2006-01-02 15:04:05" // Build timestamp in milliseconds (UTC) injected by build script
 var GitHash = "unknown"               // Git hash injected by build script
 
