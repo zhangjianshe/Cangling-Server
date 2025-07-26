11642c4deb9f12176b86342cf8e8144a881dbbdb
zhangjianshe <zhangjianshe@gmail.com>
2025-07-27 01:44:25 +0800

    Bump version to 0.0.77

--- cs-server/main.go
+++ cs-server/main.go
@@ -24,7 +24,7 @@ import (
 //go:embed static/*
 var staticFiles embed.FS
 
-var AppVersion = "0.0.76"             // Current application version
+var AppVersion = "0.0.77"             // Current application version
 var BuildTime = "2006-01-02 15:04:05" // Build timestamp in milliseconds (UTC) injected by build script
 var GitHash = "unknown"               // Git hash injected by build script
 
